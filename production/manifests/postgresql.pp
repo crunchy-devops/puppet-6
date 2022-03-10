2160841b275441f2f8b415a588f28a388193c453
Herve <dockerlite@gmail.com>
2022-03-10 16:07:34 +0100

    added postgresql

--- production/manifests/postgresql.pp
+++ production/manifests/postgresql.pp
@@ -1,22 +1,33 @@
+$dir_path = '/var/lib/pgsql/data'
 exec { 'module_postgresql':
   command => '/bin/dnf module enable postgresql:13',
 }
 
 package { 'postgresql-server':
   ensure => present,
-
 }
 
 service { 'postgresql':
     ensure => running,
+    enable => true,
 }
 
-exec { 'init_db':
-  command => '/usr/bin/postgresql-setup --initdb',
+if $dir_path {
+  notify{ "Path ${dir_path} exist": }
+}
+else {
+    exec { 'init_db':
+            command => '/usr/bin/postgresql-setup --initdb',
+         }
 }
 
-
-
+file_line { "change pg-hba.conf":
+  ensure => present,
+  match => "^# test",
+  line => ' # test formation',
+  path => '/var/lib/pgsql/data/pg_hba.conf',
+  notify => Service['postgresql'],
+}
 
 
 
